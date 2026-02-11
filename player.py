c78bf852b2ae17543a8b53ad197f924473da481b
Cjarroyo <clovis_arroyo@hotmail.com>
2026-02-11 09:36:04 -0300

    feat: added ability to shoot and some logic

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
-
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS
+from shot import Shot
 class Player(CircleShape):
     def __init__(self, x,y):
         super().__init__(x,y, PLAYER_RADIUS)
@@ -29,6 +29,8 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
 
     def move(self, dt):
@@ -36,3 +38,7 @@ class Player(CircleShape):
         rotated_vector = unit_vector.rotate(self.rotation)
         rotate_with_speed_vector= rotated_vector * PLAYER_SPEED * dt
         self.position += rotate_with_speed_vector
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
